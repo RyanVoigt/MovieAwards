a7e6b9418c667c9c8e43f17516ef9388f811516a
RyanVoigt <ryanvoigt.0618@gmail.com>
2021-01-07 20:04:22 -0500

    forcing desktop view on mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
  /* eslint-disable */ 
+ <meta name="viewport" content="width=1024"></meta>
 import React, { useState } from 'react';
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
